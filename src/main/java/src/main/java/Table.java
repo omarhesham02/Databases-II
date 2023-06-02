da49ed452ac3602ad174b4c408541811b3456b9a
Omar Hesham <106028678+omarhesham02@users.noreply.github.com>
2023-06-02 20:13:39 +0200

    Beginning of Indices with a broken project (godspeed)

---
+++ src/main/java/src/main/java/Table.java
@@ -0,0 +1,19 @@
+package src.main.java;
+
+import java.util.*;
+
+public class Table {
+    private String TableName;
+    private String ColumnName;
+    private String ColumnType;
+    private String ClusteringKey;
+    private String IndexName;
+    private String IndexType;
+    private Hashtable<String, String> min;
+    private Hashtable<String, String> max;
+    private Hashtable<String, String> ForeignKey;
+    private String ForeignTableName;;
+    private String ForeignColumnName;
+    private String[] Computed;
+
+}

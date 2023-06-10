67b30fdef8deb75b4abe4115053024a267d63812
FadluW <fadlu.w@gmail.com>
2023-06-10 22:36:39 +0200

    create table

--- DBApp.java
+++ DBApp.java
@@ -11,26 +11,27 @@ public class DBApp  {
     public static void main(String[] args) {
         DBApp dbApp = new DBApp( );
         
-        Hashtable<String, String> htblColNameType = new Hashtable<String, String>();
-        htblColNameType.put("ProductID", "java.lang.Integer");
-        htblColNameType.put("ProductName", "java.lang.String");
-        htblColNameType.put("ProductPrice", "java.lang.Double");
-        
-        Hashtable<String, String> htblColNameMin = new Hashtable<String, String>();
-        htblColNameMin.put("ProductID", "0");
-        htblColNameMin.put("ProductName", "A");
-        htblColNameMin.put("ProductPrice", "0");
-        
-        Hashtable<String, String> htblColNameMax = new Hashtable<String, String>();
-        htblColNameMax.put("ProductID", "1000");
-        htblColNameMax.put("ProductName", "ZZZZZZZZZZZ");
-        htblColNameMax.put("ProductPrice", "100000");
-        
-        Hashtable<String, String> htblForeignKeys = new Hashtable<String, String>();
-        
-        String[] computedCols = {};
-        
+        // Create table
         try {
+            Hashtable<String, String> htblColNameType = new Hashtable<String, String>();
+            htblColNameType.put("ProductID", "java.lang.Integer");
+            htblColNameType.put("ProductName", "java.lang.String");
+            htblColNameType.put("ProductPrice", "java.lang.Double");
+            
+            Hashtable<String, String> htblColNameMin = new Hashtable<String, String>();
+            htblColNameMin.put("ProductID", "0");
+            htblColNameMin.put("ProductName", "A");
+            htblColNameMin.put("ProductPrice", "0");
+            
+            Hashtable<String, String> htblColNameMax = new Hashtable<String, String>();
+            htblColNameMax.put("ProductID", "1000");
+            htblColNameMax.put("ProductName", "ZZZZZZZZZZZ");
+            htblColNameMax.put("ProductPrice", "100000");
+            
+            Hashtable<String, String> htblForeignKeys = new Hashtable<String, String>();
+            
+            String[] computedCols = {};
+            
             dbApp.createTable("Product", " ProductID ", htblColNameType, htblColNameMin, htblColNameMax, htblForeignKeys, computedCols);
         } catch (DBAppException e) {
             System.err.println("Can't Create Table.");
